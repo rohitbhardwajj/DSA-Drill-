93fdfdb6dedc919427f672d9ab04a129fb1e3ae7
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-03-18 11:15:07 +0530

    adding first q

---
+++ Check-Valid-ISBN Number or not.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main() {
+    string number = "0471958697";
+    int length = number.length();
+    int ans = 0, i = 0;
+    
+    if (length != 10) {
+        cout << "Not a ISBN number" << endl;
+        return 0;
+    }
+    while (i < length) {
+     
+        int digit = number[i] - '0';
+        ans += digit *(i+1);
+        i++;
+    }
+
+  
+    if (ans % 11 == 0) {
+        cout << "Yes, it's a ISBN number" << endl;
+    } else {
+        cout << "Not a ISBN number" << endl;
+    }
+
+    return 0;
+}
