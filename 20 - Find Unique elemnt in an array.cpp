e477c2897b978ea4a7231974480341ffcd1a9b07
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-03-21 00:56:19 +0530

    Done

---
+++ 20 - Find Unique elemnt in an array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int ans = 0;
+
+    for(int i = 0; i < n; i++) {
+        ans = ans ^ arr[i];
+    }
+
+    cout << ans;
+    return 0;
+}
