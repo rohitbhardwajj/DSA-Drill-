27910f1177af1175164bc4fd292e453cc28ba393
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-03-25 19:10:42 +0530

    Done

--- 39 - Move Zeroes.cpp
+++ 39 - Move Zeroes.cpp
@@ -19,3 +19,20 @@ public:
         
     }
 };
+
+// approach 2
+
+// class Solution {
+//     public:
+//         void moveZeroes(vector<int>& nums) {
+//             int j = 0; 
+            
+//             for (int i = 0; i < nums.size(); i++) {
+//                 if (nums[i] != 0) {
+//                     swap(nums[i], nums[j]);
+//                     j++;
+//                 }
+//             }
+//         }
+//     };
+    
