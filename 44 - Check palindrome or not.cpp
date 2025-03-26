892d1e7fccbb526bbe0adf8fcab83b3f81b09e3d
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-03-26 10:45:34 +0530

    Done

---
+++ 44 - Check palindrome or not.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the character array: ";
+    cin >> n;
+
+    char ch[n + 1];  
+    char temp[n + 1];  
+
+    cout << "Enter elements in ch array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> ch[i];
+        temp[i] = ch[i]; 
+    }
+    ch[n] = '\0';
+    temp[n] = '\0';  
+
+    int i = 0, j = n - 1;
+    while (i < j) {   
+        swap(ch[i], ch[j]);
+        i++;
+        j--;
+    }
+
+    bool flag = true;  
+    for (int i = 0; i < n; i++) {
+        if (temp[i] != ch[i]) {
+            flag = false;  
+            break;  
+        }
+    }
+
+    if (flag) {
+        cout << "Yes, it's a palindrome";
+    } else {
+        cout << "Not a palindrome";
+    }
+
+    return 0;
+}
