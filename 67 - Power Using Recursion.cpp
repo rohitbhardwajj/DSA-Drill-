72ebe5b5004c280df91e7a03d342fb23746e70e6
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-03-29 18:44:13 +0530

    Done

--- 67 - Power Using Recursion.cpp
+++ 67 - Power Using Recursion.cpp
@@ -23,3 +23,42 @@ int main() {
 
     return 0;
 }
+
+
+// approach--------------2
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int powr(int base , int power) {
+ 
+//  if(power==0) return 1; 
+//  if(power==1) return base;
+//     int ans= powr(base,power/2);
+    
+//     if(power%2==0) {
+//         return ans* ans;
+//     }else{
+//         return base*ans*ans;
+//     }
+    
+// }
+
+// int main() {
+//     int base;
+//     cout << "Enter base : ";
+//     cin >> base;
+//     int power;
+//     cout << "Enter power : ";
+//     cin >> power;
+    
+
+//     cout<<powr(base,power);
+    
+   
+
+//     return 0;
+// }
+
