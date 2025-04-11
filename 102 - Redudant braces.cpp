5b58b209b769d640a9886c350aebbc59391e8916
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-04-11 19:16:00 +0530

    Done

---
+++ 102 - Redudant braces.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool findRedundantBrackets(string &s)
+{
+    stack<char> st;
+
+    for (char ch : s) {
+        if (ch == '(' || ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            st.push(ch);
+        } 
+        else if (ch == ')') {
+            bool isRedundant = true;
+
+            while (!st.empty() && st.top() != '(') {
+                char top = st.top();
+                if (top == '+' || top == '-' || top == '*' || top == '/') {
+                    isRedundant = false; // operator mil gaya
+                }
+                st.pop();
+            }
+
+            if (!st.empty()) st.pop(); // remove opening '('
+
+            if (isRedundant) return true; // redundant bracket found
+        }
+    }
+
+    return false; // no redundant brackets
+}
