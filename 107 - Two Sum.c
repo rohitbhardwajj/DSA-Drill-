71238c92d012b2ee733bd81935fea9f3b3296891
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-04-15 22:43:44 +0530

    Done

---
+++ 107 - Two Sum.c
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    vector<int> get(vector<int>& nums, int target, int j) {
+        if (j >= nums.size()) return {};
+
+        int val = target - nums[j];
+        for (int i = 0; i < nums.size(); i++) {
+            if (val == nums[i] && i != j) {
+                return {j, i};
+            }
+        }
+        return get(nums, target, j + 1);
+    }
+
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        return get(nums, target, 0);
+    }
+};
