fc0aefea4a5f92c80d5f47e8bc4f2d523ed015b7
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-04-20 19:08:46 +0530

    Done

--- 108 - Minimum parantheses.c
+++ 108 - Minimum parantheses.c
@@ -18,3 +18,37 @@ public:
         return ans.size(); 
     }
 };
+
+
+// approach -----------2
+
+#include <bits/stdc++.h> 
+int findMinimumCost(string str) {
+    // Odd length string can never be valid
+    if (str.length() % 2 == 1) return -1;
+
+    stack<char> s;
+
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] == '{') {
+            s.push(str[i]);
+        } else {
+            if(!s.empty() && s.top() == '{') {
+                s.pop();
+            } else {
+                s.push(str[i]);
+            }
+        }
+    }
+
+    // Now stack contains only unbalanced brackets
+    int a = 0, b = 0;
+    while(!s.empty()) {
+        if(s.top() == '{') a++;
+        else b++;
+        s.pop();
+    }
+
+    // Minimum cost formula
+    return (a + 1) / 2 + (b + 1) / 2;
+}
