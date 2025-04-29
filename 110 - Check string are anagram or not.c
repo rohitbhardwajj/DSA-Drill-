27a0ecebf21d9790a592e41e1bcdf345828cfa06
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-04-29 18:55:34 +0530

    Done

--- 110 - Check string are anagram or not.c
+++ 110 - Check string are anagram or not.c
@@ -24,3 +24,38 @@ public:
         return true;
     }
 };
+
+
+
+// approach-------------------------2
+
+
+
+
+
+
+
+
+class Solution {
+    public:
+        bool isAnagram(string s, string t) {
+            if(s.length()!=t.length()) return false;
+          sort(s.begin(), s.end());
+          sort(t.begin(), t.end());
+        
+    
+        int i=0,j=0;
+    
+    
+        while(i<s.length() && j<t.length()){
+            if(s[i]!=t[j]) {
+                return false;
+                break;
+            }
+            i++;
+            j++;
+        }
+        return true;
+        }
+    };
+    
