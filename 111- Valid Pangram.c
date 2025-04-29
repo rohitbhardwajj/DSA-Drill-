c49febf2379ee2f0bca6eb6407d11f1606413640
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-04-29 19:45:45 +0530

    Done

--- 111- Valid Pangram.c
+++ 111- Valid Pangram.c
@@ -12,3 +12,25 @@ public:
       else return false;
     }
 };
+
+
+// approach 2-----------------
+
+
+class Solution {
+  public:
+      bool checkIfPangram(string sentence) {
+          bool seen[26] = {false};
+  
+          for (char c : sentence) {
+              seen[c - 'a'] = true;
+          }
+  
+          for (int i = 0; i < 26; ++i) {
+              if (!seen[i]) return false;
+          }
+  
+          return true;
+      }
+  };
+  
